2f560b31e753928faf2793693a2b34a8735fcee5
Min Heo <min103ju@gmail.com>
2021-07-07 22:55:20 +0900

    feat(view): 결과 view 그래프 출력 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,8 @@
+import view.ResultView;
+
 public class Application {
-    
+    public static void main(String[] args) {
+        System.out.println(ResultView.generateGraph());
+
+    }
 }
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,22 +1,22 @@
 package view;
 
 public class ResultView {
-    private final String ORIGIN = "  +";
-    private final int LOOP_COUNT = 24;
+    private static final String ORIGIN = "  +";
+    private static final int LOOP_COUNT = 24;
 
-    public String generateGraph() {
+    public static String generateGraph() {
         StringBuilder builder = new StringBuilder();
-        for (int i = LOOP_COUNT; i <= 1; i--) {
+        for (int i = LOOP_COUNT; i >= 1; i--) {
             builder.append(addEvenNumberY(i));
         }
 
         builder.append(ORIGIN);
 
-        for (int i = LOOP_COUNT; i <= 1; i--) {
+        for (int i = 1; i <= LOOP_COUNT; i++) {
             builder.append(addXLine());
         }
 
-        builder.append("\n0");
+        builder.append("\n 0 ");
 
         for (int i = 1; i <= LOOP_COUNT; i++) {
             builder.append(addEvenNumberX(i));
@@ -25,21 +25,28 @@ public class ResultView {
         return builder.toString();
     }
 
-    private String addEvenNumberY(int number) {
+    private static String addEvenNumberY(int number) {
         if (number % 2 == 0) {
-            return String.valueOf(number) + "|\n";
+            return addWhiteSpace(number) + String.valueOf(number) + "|\n";
         }
         return "  |\n";
     }
 
-    private String addXLine() {
-        return "--";
+    private static String addWhiteSpace(int number) {
+        if (number < 10) {
+            return " ";
+        }
+        return "";
     }
 
-    private String addEvenNumberX(int number) {
+    private static String addXLine() {
+        return "----";
+    }
+
+    private static String addEvenNumberX(int number) {
         if (number % 2 == 0) {
             return String.valueOf(number);
         }
-        return "";
+        return "    ";
     }
 }
