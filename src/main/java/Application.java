9d4082a2954fcbc6efa09b0536bcb1cb031def88
Min Heo <min103ju@gmail.com>
2021-07-10 05:13:05 +0900

    feat(resultView): 결과 화면(그래프 출력) & 포인터 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,8 @@ import view.ResultView;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println(ResultView.generateGraph());
+        System.out.println(
 
+        );
     }
 }
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,52 +1,90 @@
 package view;
 
 public class ResultView {
+    private static final String X_AXIS_WHITE_SPACE = "   ";
+    private static final String Y_AXIS_WHITE_SPACE = "  |";
+    private static final String X_AXIS_MARKER = "----";
+    private static final String Y_AXIS_MARKER = "|";
+    private static final String POINT = "*";
     private static final String ORIGIN = "  +";
+    private static final String NONE = "";
     private static final int LOOP_COUNT = 24;
 
-    public static String generateGraph() {
-        StringBuilder builder = new StringBuilder();
+    public static String generateGraph(int x, int y) {
+        StringBuilder yAxis = new StringBuilder();
+        StringBuilder xAxis = new StringBuilder();
+
         for (int i = LOOP_COUNT; i >= 1; i--) {
-            builder.append(addEvenNumberY(i));
+            yAxis.append(addyAxis(i) + checkPointCoordinate(x, y, i)+ "\n");
         }
 
-        builder.append(ORIGIN);
+        yAxis.append(ORIGIN);
+
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            xAxis.append(addxAxis());
+        }
+
+        xAxis.append("\n");
+        xAxis.append("0");
+        xAxis.append("  ");
 
         for (int i = 1; i <= LOOP_COUNT; i++) {
-            builder.append(addXLine());
+            xAxis.append(addxNumber(i));
         }
 
-        builder.append("\n 0 ");
+        yAxis.append(xAxis.toString());
 
-        for (int i = 1; i <= LOOP_COUNT; i++) {
-            builder.append(addEvenNumberX(i));
+        return yAxis.toString();
+    }
+
+    private static String addxLessTen(int number) {
+        if (number < 10) {
+            return " " + number;
         }
+        return number + NONE;
+    }
+
+    private static String addxNumber(int number) {
+        if (number % 2 == 0) {
+            return X_AXIS_WHITE_SPACE + addxLessTen(number);
+        }
+        return X_AXIS_WHITE_SPACE;
+    }
+
+    private static String addxAxis() {
+        return X_AXIS_MARKER;
+    }
+
+    private static String addyAxis(int number) {
+        if (number % 2 == 0) {
+            return addyLessTen(number);
+        }
+        return Y_AXIS_WHITE_SPACE;
+    }
+
+    private static String addyLessTen(int number) {
+        if (number < 10) {
+            return " " + number + Y_AXIS_MARKER;
+        }
+        return number + Y_AXIS_MARKER;
+    }
+
+    private static String pointCoordinate(int x) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(" ");
+        for (int i = 1; i < x; i++) {
+            builder.append(" " + X_AXIS_WHITE_SPACE);
+        }
+        builder.append(POINT);
 
         return builder.toString();
     }
 
-    private static String addEvenNumberY(int number) {
-        if (number % 2 == 0) {
-            return addWhiteSpace(number) + String.valueOf(number) + "|\n";
+    public static String checkPointCoordinate(int x, int y, int loopCount) {
+        if (y == loopCount) {
+            return pointCoordinate(x);
         }
-        return "  |\n";
-    }
-
-    private static String addWhiteSpace(int number) {
-        if (number < 10) {
-            return " ";
-        }
-        return "";
-    }
-
-    private static String addXLine() {
-        return "----";
-    }
-
-    private static String addEvenNumberX(int number) {
-        if (number % 2 == 0) {
-            return String.valueOf(number);
-        }
-        return "    ";
+        return NONE;
     }
 }
