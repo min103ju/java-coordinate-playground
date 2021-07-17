5441ca0c92258434db72f78b00b9e37d9613546b
Min Heo <min103ju@gmail.com>
2021-07-17 17:25:59 +0900

    test(distance): 두 좌표 사이의 거리 검증 테스트

--- src/test/java/coordinates/rectangle/CoordinatesRectangleTest.java
+++ src/test/java/coordinates/rectangle/CoordinatesRectangleTest.java
@@ -7,7 +7,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CoordinatesRectangleTest {
 
     @Test
-    void validateRectangle() {
+    void validateSameLineLength() {
+        // given
+        RectangleCoordinates rectangleA = new RectangleCoordinates(10, 10);
+        RectangleCoordinates rectangleB = new RectangleCoordinates(11, 11);
+
+        RectangleCoordinates rectangleC = new RectangleCoordinates(12, 12);
+        RectangleCoordinates rectangleD = new RectangleCoordinates(13, 13);
+
+        // when
+        double distanceA = rectangleA.getDistance(rectangleB);
+        double distanceB = rectangleC.getDistance(rectangleD);
+
+        // then
+        assertThat(distanceA).isEqualTo(distanceB);
+    }
+
+    @Test
+    void validateDifferPoint() {
         // given
         RectangleCoordinates rectangleA = new RectangleCoordinates(10, 10);
         RectangleCoordinates rectangleB = new RectangleCoordinates(11, 11);
